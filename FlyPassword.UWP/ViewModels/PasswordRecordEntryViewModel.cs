dc2c1f4772ec894276dc01f102b46f5a110f5ebb
yinyue200 <yinyue200@outlook.com>
2018-12-22 00:01:53 +0800

    add PasswordRecordEntryViewModel

---
+++ FlyPassword.UWP/ViewModels/PasswordRecordEntryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyPassword.UWP.ViewModels
+{
+    class PasswordRecordEntryViewModel
+    {
+        public string DisplayName { get; }
+        public string DisplayValue { get; }
+    }
+}
