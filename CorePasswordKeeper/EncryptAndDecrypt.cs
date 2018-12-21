eb2736647958ebaca506bd8721a1f70e98f19c84
yinyue200 <yinyue200@outlook.com>
2018-12-22 00:36:17 +0800

    增加加密解密机制

---
+++ CorePasswordKeeper/EncryptAndDecrypt.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace CorePasswordKeeper
+{
+    public static class EncryptAndDecrypt
+    {
+        static readonly byte[] IV = new byte[16] { 0x23, 0x7a, 0x6a, 0xff, 0xea, 0xbd, 0x4f, 0xee, 0x63, 0x70, 0x45, 0xbe, 0xae, 0xfe, 0x85, 0x99 };
+        public static void Encrypt(Stream data,string key,Stream output)
+        {
+            var bytekey = Encoding.UTF8.GetBytes(key);
+            using (var aes = GetAes())
+            {
+                aes.Key = bytekey;
+                aes.IV = IV;               
+                var enc = aes.CreateEncryptor();
+                using (var cryptoStream = new CryptoStream(output, enc, CryptoStreamMode.Write))
+                {
+                    data.CopyTo(cryptoStream);
+                }
+            }
+        }
+
+        private static Aes GetAes()
+        {
+            var aes= Aes.Create();
+            aes.BlockSize = 128;
+            aes.KeySize = 256;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+            return aes;
+        }
+
+        public static void Decrypt(Stream data, string key, Stream output)
+        {
+            var bytekey = Encoding.UTF8.GetBytes(key);
+            using (var aes = GetAes())
+            {
+                aes.Key = bytekey;
+                aes.IV = IV;
+                var enc = aes.CreateDecryptor();
+                using (var cryptoStream = new CryptoStream(data, enc, CryptoStreamMode.Read))
+                {
+                    cryptoStream.CopyTo(output);
+                }
+            }
+        }
+    }
+}
---
+++ FlyPassword.UWP/Core/DataManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using CorePasswordKeeper;
+using System.IO;
+
+namespace FlyPassword.UWP.Core
+{
+    static class DataManager
+    {       
+        public static async Task<PasswordKeeper> LoadFromFileAsync(IStorageFile storageFile,string password)
+        {
+            using(var stream= await storageFile.OpenStreamForReadAsync())
+            {
+                using(var mem=new MemoryStream())
+                {
+                    EncryptAndDecrypt.Decrypt(stream, password, mem);
+                    var passwordKeeper = new PasswordKeeper();
+                    passwordKeeper.LoadString(GetText(mem));
+                    return passwordKeeper;
+                }
+            }
+        }
+        public static async Task SaveToFileAsync(IStorageFile storageFile, string password,PasswordKeeper passwordKeeper)
+        {
+            using (var stream = await storageFile.OpenStreamForWriteAsync())
+            {
+                using (var mem = new MemoryStream(Encoding.UTF8.GetBytes(passwordKeeper.SaveToJson())))
+                {
+                    EncryptAndDecrypt.Encrypt(mem, password, stream);
+                }
+            }
+        }
+        static string GetText(Stream stream)
+        {
+            using(var textreader=new StreamReader(stream))
+            {
+                return textreader.ReadToEnd();
+            }
+        }
+    }
+}
