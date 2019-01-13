98a658b5d45987d953a9d86210944bbebcb21a35
yinyue200 <yinyue200@outlook.com>
2019-01-13 15:47:33 +0800

    add license header

--- FlyPassword.UWP/XamlCore/ProgressBarVisualHelper.cs
+++ FlyPassword.UWP/XamlCore/ProgressBarVisualHelper.cs
@@ -1,10 +1,9 @@
-﻿//PixivUniversal
-//Copyright(C) 2017 Pixeez Plus Project
+﻿//FlyPassword
+//Copyright(C) yinyue200.com 
 
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; version 2
-//of the License.
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, version 3 of the License.
 
 //This program is distributed in the hope that it will be useful,
 //but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,8 +11,7 @@
 //GNU General Public License for more details.
 
 //You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//along with this program.If not, see https://github.com/yinyue200/FlyPassword/blob/master/LICENSE.
 using System;
 using System.Collections.Generic;
 using System.Linq;
